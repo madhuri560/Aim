cb78a8a3afadf9d74c21952c0cf2e5fca177ffad
Nithya641 <nithyashree.m@s.amity.edu>
2025-03-27 19:11:39 +0530

    to check whether the given number is present in list or not

--- code.py
+++ code.py
@@ -9,3 +9,10 @@ print(a[1:4])
 a=[10,20,40,50,60]
 a[0],a[-1]=a[-1],a[0]
 print(a)
+
+a=[10,20,40,50,60]
+num=int(input("Enter a number to check"))
+if num in a:
+    print(f"{num} is present in the list:")
+else:
+    print(f"{num} is not present in the list:")
